f3ffffdccca685f3d879e269057082f3f5f90382
Ayush022007 <singhayush89076@gmail.com>
2026-09-06 01:54:44 +0530

    new

---
+++ que8.java
@@ -0,0 +1,9 @@
+package strings;
+//checking 
+public class que8 {
+    public static void main(String[] args) {
+    String str = "ApnaCollege".replace("l"," ");
+    System.out.print(str);
+    }
+   
+}
