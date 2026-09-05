4cdf892bf09dae5db76a2250114cad6c626cb498
Ayush022007 <singhayush89076@gmail.com>
2026-09-06 01:50:11 +0530

    new

---
+++ que6.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class que6 {
+    // public static String upcase(String str){
+    //     char str2;
+    //     int n = str.length();
+    //     for(int i = 0;i<n;i++){
+    //         str2 = str.upperCase(i);
+    //     }
+    //     return str2;
+    // }
+    public static void main(String[] args) {
+        String str = "hi, i am Ayush";
+        String str2 = str.toUpperCase();
+        System.out.print(str2);
+    }
+}
---
+++ que7.java
@@ -0,0 +1,10 @@
+package strings;
+//practice question
+public class que7 {
+    public static void main(String[] args) {
+        String str = "ShradhaDidi";
+        String str1 = "ApnaCollege";
+        String str2 = "ShradhaDidi";
+        System.out.println(str.equals(str1)+ " " + str.equals(str2));
+    }
+}
